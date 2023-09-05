00f621db5104e0060f92b46b53a7e0018bfa60c8
nhphero_dev <nhphero.dev@gmail.com>
2023-09-05 16:37:40 +0700

    update

--- database/migrations/2023_09_05_031619_add_teams_fields.php
+++ database/migrations/2023_09_05_031619_add_teams_fields.php
@@ -60,7 +60,7 @@ class AddTeamsFields extends Migration
 
         if (! Schema::hasColumn($tableNames['model_has_roles'], $columnNames['team_foreign_key'])) {
             Schema::table($tableNames['model_has_roles'], function (Blueprint $table) use ($tableNames, $columnNames) {
-                $table->unsignedBigInteger($columnNames['team_foreign_key'])->default('1');;
+                $table->unsignedBigInteger($columnNames['team_foreign_key'])->nullable();
                 $table->index($columnNames['team_foreign_key'], 'model_has_roles_team_foreign_key_index');
 
                 if (DB::getDriverName() !== 'sqlite') {
