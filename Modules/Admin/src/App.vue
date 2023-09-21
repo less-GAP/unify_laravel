01ae3f1b3ccd883a796b5abafb2f08f5db493d30
Phuong <phuong@lessgap.com>
2023-09-21 11:06:18 +0700

    update app.vue

--- Modules/Admin/src/App.vue
+++ Modules/Admin/src/App.vue
@@ -2,17 +2,18 @@
   <a-config-provider v-bind="$style.antdv" :getPopupContainer="getPopupContainer">
     <c-reset/>
     <a-style-provider hash-priority="high">
-      <router-view  v-slot="{ Component }">
+      <router-view v-slot="{ Component }">
         <transition>
-          <keep-alive :max="5">
-            <component :key="$route.fullPath" :is="Component"/>
+          <keep-alive :max="10">
+            <div>
+              <component :key="$route?.matched[0]?.path" :is="Component"/>
+            </div>
           </keep-alive>
         </transition>
       </router-view>
     </a-style-provider>
   </a-config-provider>
 </template>
-
 <script>
 
 export default {
