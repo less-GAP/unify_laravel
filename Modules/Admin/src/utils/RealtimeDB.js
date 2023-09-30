ede40a00911b470331e0e3bad918252988c7bc8f
Phuong <phuong@lessgap.com>
2023-09-30 12:52:44 +0700

    update build

--- Modules/Admin/src/utils/RealtimeDB.js
+++ Modules/Admin/src/utils/RealtimeDB.js
@@ -1,13 +1,9 @@
 import Surreal from "https://cdn.jsdelivr.net/npm/surrealdb.js";
-import {Subject} from 'rxjs'
 
 const db = new Surreal();
-// Connect to the database
-await db.connect( import.meta.env.VITE_RPC_HOST?import.meta.env.VITE_RPC_HOST:'http://localhost:8000/rpc', {
-  // Set the namespace and database for the connection
+db.connect(import.meta.env.VITE_RPC_HOST ? import.meta.env.VITE_RPC_HOST : 'http://localhost:8000/rpc', {
   ns: 'unify',
   db: 'unify',
-  // Set the authentication details for the connection
   auth: {
     user: 'viewer',
     pass: '123456',
