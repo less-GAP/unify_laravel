1f40d5db49f15512ede6791492dbea9d612adbc9
Phuong <phuong@lessgap.com>
2023-10-02 18:23:19 +0700

    update buid

--- Modules/Admin/src/stores/appState.js
+++ Modules/Admin/src/stores/appState.js
@@ -29,25 +29,25 @@ export const useAppStateStore = defineStore("appState", {
       this.currentRoute = currentRoute
     },
     async fetch() {
-      await db.live(
-        "versions",
-        // The callback function takes an object with the "action" and "result" properties
-        ({action, result}) => {
-          // action can be: "CREATE", "UPDATE", "DELETE" or "CLOSE"
-          if (action === "CLOSE") return;
-          // result contains either the entire record, or a set of JSON patches when diff mode is enabled
-          this.versions[result.key] = result.value
-        })
-      await db.live(
-        "statistics",
-        // The callback function takes an object with the "action" and "result" properties
-        ({action, result}) => {
-          // action can be: "CREATE", "UPDATE", "DELETE" or "CLOSE"
-          if (action === "CLOSE") return;
-          // result contains either the entire record, or a set of JSON patches when diff mode is enabled
-
-          this.statistics[result.key] = result.value
-        })
+      // await db.live(
+      //   "versions",
+      //   // The callback function takes an object with the "action" and "result" properties
+      //   ({action, result}) => {
+      //     // action can be: "CREATE", "UPDATE", "DELETE" or "CLOSE"
+      //     if (action === "CLOSE") return;
+      //     // result contains either the entire record, or a set of JSON patches when diff mode is enabled
+      //     this.versions[result.key] = result.value
+      //   })
+      // await db.live(
+      //   "statistics",
+      //   // The callback function takes an object with the "action" and "result" properties
+      //   ({action, result}) => {
+      //     // action can be: "CREATE", "UPDATE", "DELETE" or "CLOSE"
+      //     if (action === "CLOSE") return;
+      //     // result contains either the entire record, or a set of JSON patches when diff mode is enabled
+      //
+      //     this.statistics[result.key] = result.value
+      //   })
       Api.get("config", {params: {names: Object.keys(defaultConfig)}})
         .then((rs) => {
           this.configs = rs.data
