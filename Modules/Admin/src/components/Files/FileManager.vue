3527718390ba57f564863fb9320ba96301502a5b
Nguyen Pham <admin@madelab.dev>
2023-10-24 09:27:51 -0500

    FIx file local view

--- Modules/Admin/src/components/Files/FileManager.vue
+++ Modules/Admin/src/components/Files/FileManager.vue
@@ -1,11 +1,11 @@
 <script setup>
-import {computed, ref, h, toRaw} from "vue";
-import {useMainStore} from "@/stores/main";
-import {mdiEye, mdiTrashCan} from "@mdi/js";
-import {Button, Input} from "@/components";
+import { computed, ref, h, toRaw } from "vue";
+import { useMainStore } from "@/stores/main";
+import { mdiEye, mdiTrashCan } from "@mdi/js";
+import { Button, Input } from "@/components";
 import BaseIcon from "@/components/BaseIcon.vue";
 import FileDetailForm from "./FileDetailForm.vue";
-import {DownOutlined,FileOutlined, FolderOpenOutlined, ReloadOutlined, UploadOutlined} from "@ant-design/icons-vue";
+import { DownOutlined, FileOutlined, FolderOpenOutlined, ReloadOutlined, UploadOutlined } from "@ant-design/icons-vue";
 import Api from "@/utils/Api";
 
 const props = defineProps({
@@ -133,6 +133,15 @@ async function onFilesSelect(option) {
 function isImageUrl(url) {
   return /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(url);
 }
+
+function getFullPath(url) {
+  const host = window.location.host
+  if (host.indexOf('9200') > -1) {
+    return "//" + host.split(':')[0] + ":9100/" + url
+  }
+  return "//" + host + "/" + url
+}
+
 reload()
 </script>
 
@@ -142,33 +151,20 @@ reload()
     <div :loading="loading" class="flex items-center justify-between">
       <a-space>
 
-        <a-input-search
-          v-if="tableConfig.globalSearch"
-          allow-clear
-          @keyup.enter="reload"
-          style="max-width: 300px"
-          v-model:value="search"
-          placeholder="Enter to search..."
-          :loading="loading"
-        />
-        <a-upload
-          name="file"
-          :showUploadList="false"
-          :max-count="10"
-          multiple="true"
-          :customRequest="onFilesSelect"
-        >
+        <a-input-search v-if="tableConfig.globalSearch" allow-clear @keyup.enter="reload" style="max-width: 300px"
+          v-model:value="search" placeholder="Enter to search..." :loading="loading" />
+        <a-upload name="file" :showUploadList="false" :max-count="10" multiple="true" :customRequest="onFilesSelect">
           <a-button>
             <upload-outlined></upload-outlined>
             Click to Upload
           </a-button>
         </a-upload>
-<!--        <a-upload :showUploadList="false" :customRequest="onFilesSelect" directory>-->
-<!--          <a-button>-->
-<!--            <FolderOpenOutlined></FolderOpenOutlined>-->
-<!--            Upload Directory-->
-<!--          </a-button>-->
-<!--        </a-upload>-->
+        <!--        <a-upload :showUploadList="false" :customRequest="onFilesSelect" directory>-->
+        <!--          <a-button>-->
+        <!--            <FolderOpenOutlined></FolderOpenOutlined>-->
+        <!--            Upload Directory-->
+        <!--          </a-button>-->
+        <!--        </a-upload>-->
       </a-space>
       <span>
 
@@ -177,14 +173,13 @@ reload()
       <a-space>
         <a-button>
           <template #icon>
-            <reload-outlined @click="reload"/>
+            <reload-outlined @click="reload" />
           </template>
         </a-button>
         <a-dropdown v-if="selectionActions.length > 0" :disabled="!selectedItems.length">
           <template #overlay>
             <a-menu>
-              <a-menu-item @click="doSelectionAction(action)" :key="index" v-for="(action,index) in selectionActions"
-              >
+              <a-menu-item @click="doSelectionAction(action)" :key="index" v-for="(action, index) in selectionActions">
                 {{ action.title }}
               </a-menu-item>
 
@@ -192,36 +187,29 @@ reload()
           </template>
           <a-button>
             Hành động
-            <DownOutlined/>
+            <DownOutlined />
           </a-button>
         </a-dropdown>
 
-        <a-button type="primary" v-if="addAction" @click="()=>{addAction(reload)}">Thêm mới</a-button>
+        <a-button type="primary" v-if="addAction" @click="() => { addAction(reload) }">Thêm mới</a-button>
 
       </a-space>
     </div>
     <div class="mt-12 mb-12 grid grid-cols-2 md:grid-cols-6  gap-4">
       <div v-for="file in inProgressFiles"
-           class="cursor-pointer border-2 border-gray-100  hover:border-blue-700  h-[150px] mb-5  rounded-lg overflow:hidden">
-        <a-progress type="circle" :percent="file.percent"/>
+        class="cursor-pointer border-2 border-gray-100  hover:border-blue-700  h-[150px] mb-5  rounded-lg overflow:hidden">
+        <a-progress type="circle" :percent="file.percent" />
       </div>
       <div @click="fileDetail = image" v-for="image in tableData.data" :key="image.id"
-           class=" cursor-pointer relative border-2 border-gray-100 hover:border-blue-700 h-[150px]  mb-[50px] rounded-lg overflow:hidden">
-        <img
-          v-if="isImageUrl(image.file_url)"
-          style="max-width:auto;max-height:auto"
-          :title="image.file_name"
-          class="h-full object-contain object-center w-full max-w-full "
-          :src="image.file_url"
-        />
-        <div
-          v-else
-          :title="image.file_name"
-          class="h-full flex items-center text-center object-contain object-center w-full max-w-full "
-        >
+        class=" cursor-pointer relative border-2 border-gray-100 hover:border-blue-700 h-[150px]  mb-[50px] rounded-lg overflow:hidden">
+        <img v-if="isImageUrl(image.file_url)" style="max-width:auto;max-height:auto" :title="image.file_name"
+          class="h-full object-contain object-center w-full max-w-full " :src="getFullPath(image.site_path)" />
+        <div v-else :title="image.file_name"
+          class="h-full flex items-center text-center object-contain object-center w-full max-w-full ">
           <file-outlined style="margin:0 auto;font-size: 30px" />
         </div>
-        <div class="absolute whitespace-nowrap text-center overflow-hidden text-ellipsis -bottom-[40px] bg-white w-full pl-2 pr-2 ">
+        <div
+          class="absolute whitespace-nowrap text-center overflow-hidden text-ellipsis -bottom-[40px] bg-white w-full pl-2 pr-2 ">
           {{ image.file_name }}
         </div>
       </div>
@@ -234,8 +222,7 @@ reload()
       </div>
     </div>
     <a-pagination v-if="pagination?.total" :showSizeChanger="showSizeChanger" @change="reload"
-                  v-model:current="pagination.page"
-                  v-model:pageSize="pagination.perPage" :total="pagination.total">
+      v-model:current="pagination.page" v-model:pageSize="pagination.perPage" :total="pagination.total">
       <template #itemRender="{ type, originalElement }">
         <a v-if="type === 'prev'">Previous</a>
         <a v-else-if="type === 'next'">Next</a>
@@ -243,20 +230,13 @@ reload()
       </template>
     </a-pagination>
   </div>
-  <a-drawer
-    style="max-width: 100vw"
-    appendTo="body"
-    v-model:open="fileDetail"
-    class="custom-class"
-    width="800px"
-    title="File Detail"
-    placement="right"
-  >
-    <FileDetailForm @delete="fileDetail = false;reload()" :value="fileDetail"></FileDetailForm>
+  <a-drawer style="max-width: 100vw" appendTo="body" v-model:open="fileDetail" class="custom-class" width="800px"
+    title="File Detail" placement="right">
+    <FileDetailForm @delete="fileDetail = false; reload()" :value="fileDetail"></FileDetailForm>
   </a-drawer>
 </template>
 <style scoped>
-.item-actions > :not(:first-child) {
+.item-actions> :not(:first-child) {
   margin-left: 10px
 }
 </style>
