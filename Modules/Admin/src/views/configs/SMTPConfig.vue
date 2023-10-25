e3ce989c99f03ca3557bd388f2ad556c41a1fc9c
Nguyen Pham <admin@madelab.dev>
2023-10-25 15:19:55 -0500

    Update config

--- Modules/Admin/src/views/configs/SMTPConfig.vue
+++ Modules/Admin/src/views/configs/SMTPConfig.vue
@@ -1,10 +1,10 @@
 <script setup>
-import {reactive, ref} from "vue";
-import {message} from 'ant-design-vue';
+import { reactive, ref } from "vue";
+import { message } from 'ant-design-vue';
 
 import Api from "@/utils/Api";
 import validateMessages from "@/utils/validateMessages";
-import {InputUpload} from "@/components";
+import { InputUpload } from "@/components";
 
 const configNames = [
   'smtp_server',
@@ -27,13 +27,13 @@ const formConfig = reactive({
   "validateTrigger": "submit",
   "label-align": "top",
   "model": formState,
-  labelCol: {span: 24},
-  wrapperCol: {span: 24},
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
   "validate-messages": validateMessages,
 });
 const fetch = function () {
   loading.value = true
-  Api.get('config', {params: {names: configNames}}).then(result => {
+  Api.get('config', { params: { names: configNames } }).then(result => {
     Object.assign(formState, result.data)
   }).catch(e => {
   }).finally(loading.value = false)
@@ -47,7 +47,7 @@ const submit = async function () {
   }).finally(loading.value = false)
 }
 const sendTest = async function () {
-  Api.post('config/testSmtp', {...formState, ...formTest}).then(result => {
+  Api.post('config/testSmtp', { ...formState, ...formTest }).then(result => {
     openTest.value = false
   }).catch(e => {
   }).finally(loading.value = false)
@@ -61,60 +61,41 @@ const cancel = function () {
 </script>
 
 <template>
+  <h3 class="font-bold">SMTP Config</h3>
+  <hr class="mb-6 mt-4" />
+  <a-form style="max-width: 600px" autocomplete="off" :model="formState" v-bind="formConfig" @finish="submit">
+    <a-form-item name="smtp_server" label="SMTP Server" :rules="[{ required: true }]">
+      <a-input placeholder="domain.com" autocomplete="off" v-model:value="formState.smtp_server" />
+    </a-form-item>
+    <a-form-item name="smtp_port" label="SMTP Port" :rules="[{ required: true }]">
+      <a-input placeholder="25" autocomplete="off" v-model:value="formState.smtp_port" />
+    </a-form-item>
+    <a-form-item name="smtp_security" label="Security" :rules="[{ required: true }]">
+      <a-input placeholder="tls" autocomplete="off" v-model:value="formState.smtp_security" />
+    </a-form-item>
 
-  <a-form
-    autocomplete="off"
-    :model="formState"
-    v-bind="formConfig"
-    @finish="submit"
-  >
-    <a-row class="w-full">
-      <a-form-item class="md:w-[200px] w-full mr-5" name="smtp_server" label="SMTP Server"
-                   :rules="[{ required: true }]">
-        <a-input placeholder="domain.com" autocomplete="off" v-model:value="formState.smtp_server"/>
-      </a-form-item>
-      <a-form-item class="md:w-[200px] w-full mr-5" name="smtp_port" label="SMTP Port" :rules="[{ required: true }]">
-        <a-input placeholder="25" autocomplete="off" v-model:value="formState.smtp_port"/>
-      </a-form-item>
-      <a-form-item class="md:w-[200px] w-full mr-5" name="smtp_security" label="Security" :rules="[{ required: true }]">
-        <a-input placeholder="tls" autocomplete="off" v-model:value="formState.smtp_security"/>
-      </a-form-item>
-    </a-row>
+    <a-form-item name="smtp_user" label="User" :rules="[{ required: true }]">
+      <a-input placeholder="User" autocomplete="off" v-model:value="formState.smtp_user" />
+    </a-form-item>
+    <a-form-item name="smtp_password" label="Password" :rules="[{ required: true }]">
+      <a-input placeholder="Password" type="password" autocomplete="off" v-model:value="formState.smtp_password" />
+    </a-form-item>
+    <a-form-item name="smtp_email_from" label="From email" :rules="[{ required: true }]">
+      <a-input placeholder="test@example.com" autocomplete="off" v-model:value="formState.smtp_email_from" />
+    </a-form-item>
 
-    <a-row class="w-full">
-      <a-form-item class="md:w-[200px] w-full mr-5" name="smtp_user" label="User" :rules="[{ required: true }]">
-        <a-input placeholder="User" autocomplete="off" v-model:value="formState.smtp_user"/>
-      </a-form-item>
-      <a-form-item class="md:w-[200px] w-full mr-5" name="smtp_password" label="Password" :rules="[{ required: true }]">
-        <a-input placeholder="Password" type="password" autocomplete="off" v-model:value="formState.smtp_password"/>
-      </a-form-item>
-    </a-row>
-    <a-row class="w-full">
-      <a-form-item class="md:w-[200px] w-full mr-5" name="smtp_email_from" label="From email"
-                   :rules="[{ required: true }]">
-        <a-input placeholder="test@example.com" autocomplete="off" v-model:value="formState.smtp_email_from"/>
-      </a-form-item>
-    </a-row>
-
-    <a-form-item>
+    <a-form-item :wrapper-col="{ ...formConfig.wrapperCol, offset: 8 }">
       <a-space>
         <a-button :loading="loading" type="primary" html-type="submit">Submit</a-button>
-        <a-button @click.prevent="openTest =true">Test</a-button>
+        <a-button @click.prevent="openTest = true">Test</a-button>
       </a-space>
     </a-form-item>
   </a-form>
   <a-modal v-model:open="openTest" title="Send Test Email" @ok="sendTest">
-    <a-form
-      class="p-8"
-      autocomplete="off"
-      v-bind="formConfig"
-      :model="formTest"
-
-    >
+    <a-form class="p-8" autocomplete="off" v-bind="formConfig" :model="formTest">
       <a-form-item class="w-full" name="email_to" label="Send to email" :rules="[{ required: true }]">
-        <a-input placeholder="test@example.com" autocomplete="off" v-model:value="formTest.email_to"/>
+        <a-input placeholder="test@example.com" autocomplete="off" v-model:value="formTest.email_to" />
       </a-form-item>
-
     </a-form>
   </a-modal>
 </template>
