2198c24c0901ee8030964eb29448a725788d4b31
Nguyen Pham <admin@madelab.dev>
2023-10-31 11:39:56 -0500

    Progress Bar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,33 @@
 import "./bootstrap";
 import "../css/app.css";
 import "@protonemedia/laravel-splade/dist/style.css";
-import { createPinia } from "pinia";
+import {
+    createPinia
+} from "pinia";
 import Antd from 'ant-design-vue';
-import { createApp } from "vue/dist/vue.esm-bundler.js";
-import { renderSpladeApp, SpladePlugin } from "@protonemedia/laravel-splade";
+import {
+    createApp
+} from "vue/dist/vue.esm-bundler.js";
+import {
+    renderSpladeApp,
+    SpladePlugin
+} from "@protonemedia/laravel-splade";
 const pinia = createPinia();
 import FrontendNewPatientForm from './components/FrontendNewPatientForm.vue';
 const el = document.getElementById("app");
 const app = createApp({
-    render: renderSpladeApp({ el }),
-}).use(SpladePlugin, {
-    'max_keep_alive': 50,
-    'transform_anchors': true,
-}).use(pinia).use(Antd)
+        render: renderSpladeApp({
+            el
+        }),
+    }).use(SpladePlugin, {
+        'max_keep_alive': 50,
+        'transform_anchors': true,
+        'progress_bar': {
+            delay: 250,
+            color: "#4B5563",
+            css: true,
+            spinner: true,
+        }
+    }).use(pinia).use(Antd)
     .component('FrontendNewPatientForm', FrontendNewPatientForm)
     .mount(el)
